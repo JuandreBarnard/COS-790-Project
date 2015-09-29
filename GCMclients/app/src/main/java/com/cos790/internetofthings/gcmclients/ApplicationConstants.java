46d79c89ef1c89806c6e9c467db40797745e08b0
unknown <xciverb@gmail.com>
2015-09-29 17:43:36 +0200

    GCM server to client

---
+++ GCMclients/app/src/main/java/com/cos790/internetofthings/gcmclients/ApplicationConstants.java
@@ -0,0 +1,16 @@
+package com.cos790.internetofthings.gcmclients;
+
+
+public interface ApplicationConstants {
+
+    // Php Application URL to store Reg ID created
+    //static final String APP_SERVER_URL = "http://10.0.2.1/test/gcm.php?shareRegId=true";
+    static final String APP_SERVER_URL = "http://41.53.88.105/test/gcm.php?shareRegId=true";
+    //static final String APP_SERVER_URL = "http://localhost/test/gcm.php?shareRegId=true";
+    // Google Project Number
+    static final String GOOGLE_PROJ_ID = "637250516433";
+    // Message Key
+    static final String MSG_KEY = "m";
+
+}
+
---
+++ GCMclients/app/src/main/java/com/cos790/internetofthings/gcmclients/GCMNotificationIntentService.java
@@ -0,0 +1,101 @@
+package com.cos790.internetofthings.gcmclients;
+
+import android.app.AlertDialog;
+import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+//import com.google.android.gcm.GCMBaseIntentService;
+
+
+public class GCMNotificationIntentService extends IntentService {
+    // Sets an ID for the notification, so it can be updated
+    public static final int notifyID = 9001;
+    NotificationCompat.Builder builder;
+
+    public GCMNotificationIntentService() {
+        super("GcmIntentService");
+    }
+
+    public void showm(String messag)
+    {
+        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        alertDialog.setTitle("Alert");
+        alertDialog.setMessage(messag);
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.show();
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Bundle extras = intent.getExtras();
+        //extras.
+        GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
+
+        String messageType = gcm.getMessageType(intent);
+
+
+        //showm(m);
+        if (!extras.isEmpty()) {
+            if (GoogleCloudMessaging.MESSAGE_TYPE_SEND_ERROR
+                    .equals(messageType)) {
+                sendNotification("Send error: " + extras.toString());
+            } else if (GoogleCloudMessaging.MESSAGE_TYPE_DELETED
+                    .equals(messageType)) {
+                sendNotification("Deleted messages on server: "
+                        + extras.toString());
+            } else if (GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE
+                    .equals(messageType)) {
+                sendNotification("Message Received from Google GCM Server:\n\n"
+                        + extras.getString(ApplicationConstants.MSG_KEY));
+            }
+        }
+        GcmBroadcastReceiver.completeWakefulIntent(intent);
+    }
+
+    private void sendNotification(String msg) {
+        Intent resultIntent = new Intent(this, HomeActivity.class);
+        resultIntent.putExtra("msg", msg);
+        PendingIntent resultPendingIntent = PendingIntent.getActivity(this, 0,
+                resultIntent, PendingIntent.FLAG_ONE_SHOT);
+
+        NotificationCompat.Builder mNotifyBuilder;
+        NotificationManager mNotificationManager;
+
+        mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        mNotifyBuilder = new NotificationCompat.Builder(this)
+                .setContentTitle("Alert")
+                .setContentText("You've received new message.")
+                .setSmallIcon(R.mipmap.ic_launcher);
+        // Set pending intent
+        mNotifyBuilder.setContentIntent(resultPendingIntent);
+
+        // Set Vibrate, Sound and Light
+        int defaults = 0;
+        defaults = defaults | Notification.DEFAULT_LIGHTS;
+        defaults = defaults | Notification.DEFAULT_VIBRATE;
+        defaults = defaults | Notification.DEFAULT_SOUND;
+
+        mNotifyBuilder.setDefaults(defaults);
+        // Set the content for Notification
+        mNotifyBuilder.setContentText("New message from Server");
+        // Set autocancel
+        mNotifyBuilder.setAutoCancel(true);
+        // Post a notification
+        mNotificationManager.notify(notifyID, mNotifyBuilder.build());
+    }
+}
