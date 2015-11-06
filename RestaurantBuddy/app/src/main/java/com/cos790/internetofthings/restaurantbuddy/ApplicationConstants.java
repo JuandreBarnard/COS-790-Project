448fb8a0bff110b53c7220ca8a6d67e9562d312a
Xciver <xciverb@gmail.com>
2015-11-06 17:07:44 +0200

    Constant file

---
+++ RestaurantBuddy/app/src/main/java/com/cos790/internetofthings/restaurantbuddy/ApplicationConstants.java
@@ -0,0 +1,16 @@
+package com.cos790.internetofthings.restaurantbuddy;
+
+
+public interface ApplicationConstants {
+
+    // Php Application URL to store Reg ID created
+    static final String APP_SERVER_URL_USER = "http://192.168.43.21:8080/test/gcm.php?User=true";
+    //static final String APP_SERVER_URL_USER = "http://restaurantbuddy.ddns.net:8080/test/gcm.php?User=true";
+    //static final String APP_SERVER_URL = "http://localhost/test/gcm.php?shareRegId=true";
+    // Google Project Number
+    static final String GOOGLE_PROJ_ID = "637250516433";
+    // Message Key
+    static final String MSG_KEY = "m";
+
+}
+
