b9ce60355fd3266e88f9830d79dfee689ff748d8
Xciver <xciverb@gmail.com>
2015-11-06 17:05:36 +0200

    Login updates

--- RestaurantBuddy/app/src/main/java/com/cos790/internetofthings/restaurantbuddy/LoginActivity.java
+++ RestaurantBuddy/app/src/main/java/com/cos790/internetofthings/restaurantbuddy/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.cos790.internetofthings.restaurantbuddy;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.Looper;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,11 +10,25 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+
 
 public class LoginActivity extends ActionBarActivity {
 
     public final static String USERNAME = "com.cos790.internetofthings.restaurantbuddy.LoginActivity.USERNAME";
-
+    public final static String PASSWORD = "com.cos790.internetofthings.restaurantbuddy.LoginActivity.PASSWORD";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,11 +45,12 @@ public class LoginActivity extends ActionBarActivity {
         EditText passwordEditText = (EditText) findViewById(R.id.password);
         String password = passwordEditText.getText().toString();
 
-        Boolean result = authenticate(username, password);
+        Boolean result = sendJson(username, password);
 
         if (result) {
             Intent intent = new Intent(this, WelcomeActivity.class);
             intent.putExtra(USERNAME, username);
+            intent.putExtra(PASSWORD, username);
             startActivity(intent);
         }
     }
@@ -41,6 +58,7 @@ public class LoginActivity extends ActionBarActivity {
     // Authenticate
     // TODO: post(json)
     public boolean authenticate(String username, String password) {
+
         String json = "{\" type \": \" SUCCESS \", \"message\": \"Successfully logged in!\", \" data \": { \"username\" " + username + ", \"passowrd\": " + password + " }}";
         return true;
     }
@@ -66,4 +84,47 @@ public class LoginActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    Context applicationContext;
+    protected boolean sendJson(final String email, final String pwd) {
+        applicationContext = getApplicationContext();
+        Thread t = new Thread() {
+
+            public void run() {
+                Looper.prepare(); //For Preparing Message Pool for the child Thread
+                HttpClient client = new DefaultHttpClient();
+                HttpConnectionParams.setConnectionTimeout(client.getParams(), 10000); //Timeout Limit
+                HttpResponse response;
+                JSONObject json = new JSONObject();
+
+                try {
+                    HttpPost post = new HttpPost(ApplicationConstants.APP_SERVER_URL_USER);
+                    json.put("email", email);
+                    json.put("password", pwd);
+                    StringEntity se = new StringEntity( json.toString());
+                    se.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+                    post.setEntity(se);
+                    response = client.execute(post);
+
+                    /*Checking response */
+                    if(response!=null){
+                        InputStream in = response.getEntity().getContent(); //Get the data in the entity
+                        Toast.makeText(applicationContext,
+                                "user has been submitted",
+                                Toast.LENGTH_LONG).show();
+                    }
+
+                } catch(Exception e) {
+                    e.printStackTrace();
+
+                    //createDialog("Error", "Cannot Estabilish Connection");
+                }
+
+                Looper.loop(); //Loop in the message queue
+            }
+        };
+
+        t.start();
+        return true;
+    }
+
 }
