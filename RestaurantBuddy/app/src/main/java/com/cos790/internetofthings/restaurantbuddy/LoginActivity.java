dde2220b769e9019657976a5979c5a6d9f346348
Xciver <xciverb@gmail.com>
2015-11-16 10:25:57 +0200

    Integration

--- RestaurantBuddy/app/src/main/java/com/cos790/internetofthings/restaurantbuddy/LoginActivity.java
+++ RestaurantBuddy/app/src/main/java/com/cos790/internetofthings/restaurantbuddy/LoginActivity.java
@@ -1,19 +1,40 @@
 package com.cos790.internetofthings.restaurantbuddy;
 
-import android.content.Intent;
+/*import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.Button;
+import android.widget.EditText;*/
+import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
 import org.json.JSONObject;
 
-public class LoginActivity extends ActionBarActivity {
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-    public final static String USERNAME = "com.cos790.internetofthings.restaurantbuddy.LoginActivity.USERNAME";
+//import org.json.JSONObject;
+
+public class LoginActivity extends Activity implements OnClickListener {
+
+    /*public final static String USERNAME = "com.cos790.internetofthings.restaurantbuddy.LoginActivity.USERNAME";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,11 +66,11 @@ public class LoginActivity extends ActionBarActivity {
     public boolean authenticate(String username, String password) {
         JSONObject params = new JSONObject();
         String json = "{\" type \": \" SUCCESS \", \"message\": \"Successfully logged in!\", \" data \": { \"username\" " + username + ", \"passowrd\": " + password + " }}";
+*/
+        /*ApplicationRequest ar = new ApplicationRequest();
+        ar.server_request(params);*/
 
-        ApplicationRequest ar = new ApplicationRequest();
-        ar.server_request(params);
-
-        return true;
+        /*return true;
     }
 
     @Override
@@ -72,5 +93,128 @@ public class LoginActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    */
+
+    public final static String USERNAME = "com.cos790.internetofthings.restaurantbuddy.RegisterActivity.USERNAME";
+    private ProgressDialog pDialog;
+    JSONParser jsonParser = new JSONParser();
+    private static final String TAG_SUCCESS = "type";
+    private static final String TAG_MESSAGE = "message";
+    private EditText user, pass;
+    private Button mSubmit,mRegister;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //setup input fields
+        user = (EditText)findViewById(R.id.username);
+        pass = (EditText)findViewById(R.id.password);
+
+        //setup buttons
+        mSubmit = (Button)findViewById(R.id.login_button_submit);
+        //mRegister = (Button)findViewById(R.id.register);
+
+        //register listeners
+        mSubmit.setOnClickListener(this);
+        //mRegister.setOnClickListener(this);
+
+    }
+    String username;
+    String password;
+    @Override
+    public void onClick(View v) {
+        // TODO Auto-generated method stub
+       /* switch (v.getId()) {
+            case R.id.login:*/
+                 username = user.getText().toString();
+                 password = pass.getText().toString();
+                new AttemptLogin().execute();
+                //break;
+            /*case R.id.register:
+                Intent i = new Intent(this, RegisterActivity.class);
+                startActivity(i);
+                break;
+
+            default:
+                break;}*/
+
+    }
+
+    class AttemptLogin extends AsyncTask<String, String, String> {
+
+        /**
+         * Before starting background thread Show Progress Dialog
+         * */
+        boolean failure = false;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(LoginActivity.this);
+            pDialog.setMessage("Attempting login...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            pDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... args) {
+            // TODO Auto-generated method stub
+            // Check for success tag
+            String success;
+
+            try {
+                // Building Parameters
+                List<NameValuePair> params = new ArrayList<NameValuePair>();
+                params.add(new BasicNameValuePair("email", username));
+                params.add(new BasicNameValuePair("password", password));
+
+                Log.d("request!", "starting");
+                // getting product details by making HTTP request
+                JSONObject json = jsonParser.makeHttpRequest(
+                        ApplicationConstants.APP_SERVER_LOGIN, "POST", params);
+
+                // check your log for json response
+                Log.d("Login attempt", json.toString());
+
+                // json success tag
+                success = json.getString(TAG_SUCCESS);
+                if (success.equals("SUCCESS")) {
+                    Intent intent = new Intent(getBaseContext(), WelcomeActivity.class);
+                    intent.putExtra(USERNAME, username);
+
+                    startActivity(intent);
+                    Log.d("Login Successful!", json.toString());
+                    //Intent i = new Intent(WelcomeActivity.this, ReadComments.class);
+                    finish();
+                    //startActivity(i);
+                    return json.getString(TAG_SUCCESS);
+                }else{
+                    Log.d("Login Failure!", json.getString(TAG_MESSAGE));
+                    return json.getString(TAG_MESSAGE);
+
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+
+        }
+        /**
+         * After completing background task Dismiss the progress dialog
+         * **/
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog once product deleted
+            pDialog.dismiss();
+            if (file_url != null){
+                Toast.makeText(LoginActivity.this, file_url, Toast.LENGTH_LONG).show();
+            }
+
+        }
+
     }
 }
