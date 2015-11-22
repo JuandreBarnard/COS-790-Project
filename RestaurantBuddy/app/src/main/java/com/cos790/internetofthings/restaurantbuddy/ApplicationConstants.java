989f373b97d794020b59bbf1e2d1b5ffefdd5b56
Xciver <xciverb@gmail.com>
2015-11-22 13:55:36 +0200

    welcome activity update

--- RestaurantBuddy/app/src/main/java/com/cos790/internetofthings/restaurantbuddy/ApplicationConstants.java
+++ RestaurantBuddy/app/src/main/java/com/cos790/internetofthings/restaurantbuddy/ApplicationConstants.java
@@ -13,6 +13,10 @@ public interface ApplicationConstants {
     static final String APP_SERVER_user_by_id = "http://restaurantbuddy.ddns.net:8080/RBServer/api/private/users/get_user_by_id.api.php";
     static final String APP_SERVER_get_user_by_email = "http://restaurantbuddy.ddns.net:8080/RBServer/api/private/users/get_user_by_email.api.php";
     static final String APP_SERVER_create_user = "http://restaurantbuddy.ddns.net:8080/RBServer/api/private/users/create_user.api.php";
+    static final String APP_SERVER_create_user_place = "http://restaurantbuddy.ddns.net:8080/RBServer/api/private/users/create_user_place.api.php";
+    static final String APP_SERVER_delete_user_place = "http://restaurantbuddy.ddns.net:8080/RBServer/api/private/restaurant/delete_user_place.api.php";
+    static final String APP_SERVER_submit_gps = "http://restaurantbuddy.ddns.net:8080/RBServer/api/private/restaurant/submit_gps.api.php";
+    static final String APP_SERVER_get_delivery_man_gps = "http://restaurantbuddy.ddns.net:8080/RBServer/api/private/restaurant/get_delivery_man_gps.api.php";
     // Google Project Number create_user.api.php
     static final String GOOGLE_PROJ_ID = "golden-cosmos-112605";
     // Message Key
