cafa26f8b5a15adbd76a14857472d178a085aade
Xciver <xciverb@gmail.com>
2015-11-23 10:10:51 +0200

    Final app finishes

--- RestaurantBuddy/app/src/main/java/com/cos790/internetofthings/restaurantbuddy/LoginActivity.java
+++ RestaurantBuddy/app/src/main/java/com/cos790/internetofthings/restaurantbuddy/LoginActivity.java
@@ -144,7 +144,7 @@ public class LoginActivity extends Activity implements OnClickListener {
                         intent.putExtra(ID, data.getString("id"));
                         //intent.putExtra(USERNAME, username);
                         startActivity(intent);
-                        finish();
+
                     }
                     Log.d("Login Successful!", json.toString());
                     //Intent i = new Intent(WelcomeActivity.this, ReadComments.class);
